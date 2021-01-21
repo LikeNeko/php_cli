4fb5f53f61b06ef693d49b7a85c89be8add4133a
neko <1330040125@qq.com>
2021-01-21 18:20:09 +0800

    feat(add): 判断是不是对象，是对象用var_export()打印

--- Readme.md
+++ Readme.md
@@ -11,4 +11,18 @@
  * Log::wins("this is a data");
  * Log::note("this is a data");
  * Log::warn("this is a data");
+
+ * Log::fail(new A());
+ [fail][无上级方法] ⤵️
+ A::__set_state(array(
+    'b' => 1,
+    'c' => 2,
+    'd' => 3,
+ ))
+ * Log::wins(['a,','b','d']);
+ array (
+   0 => 'a,',
+   1 => 'b',
+   2 => 'd',
+ )
 ```
--- src/Log.php
+++ src/Log.php
@@ -10,18 +10,35 @@ namespace Neko\Cli;
  * @method static fail($text)
  */
 class Log{
+    static $func = true;
+
     public function __call($name, $arguments)
     {
         self::_call($name,$arguments);
     }
 
-    private function _call($name,$arguments)
+    private static function _call($name,$arguments)
     {
-        Cli::$name("[{$name}]".$arguments[0]);
+        $func = '';
+        if (self::$func){
+            $info = debug_backtrace();
+            $func = '[无上级方法]';
+            if (isset($info[2])){
+                ['file'=>$file_path,'line'=>$line,'function'=>$function,'args'=>$args] = $info[2];
+                $func = "[func:{$function}]";
+            }
+        }
+        $data = $arguments[0];
+        if (!is_string($arguments[0])&&!is_numeric($arguments[0])){
+            $data = "⤵️".PHP_EOL.var_export($arguments[0],true);
+        }
+
+        Cli::$name("[{$name}]{$func} ".$data);
     }
 
     public static function __callStatic($name, $arguments)
     {
         self::_call($name,$arguments);
     }
+
 }
