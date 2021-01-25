4c7014408ed12b5b65f811ae1000737afac24243
neko <1330040125@qq.com>
2021-01-25 21:12:41 +0800

    feat(add): 判断是不是对象，是对象用var_export()打印,加入详细判断

--- src/Log.php
+++ src/Log.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Neko\Cli;
 
 /**
@@ -9,36 +10,60 @@ namespace Neko\Cli;
  * @method static wins($text)
  * @method static fail($text)
  */
-class Log{
+class Log
+{
     static $func = true;
 
     public function __call($name, $arguments)
     {
-        self::_call($name,$arguments);
+        self::_call($name, $arguments);
     }
 
-    private static function _call($name,$arguments)
+    private static function _call($name, $arguments)
     {
         $func = '';
-        if (self::$func){
+        if (self::$func) {
             $info = debug_backtrace();
             $func = '[无上级方法]';
-            if (isset($info[2])){
-                ['file'=>$file_path,'line'=>$line,'function'=>$function,'args'=>$args] = $info[2];
-                $func = "[func:{$function}]";
+
+            if (isset($info[2])) {
+                ['file' => $file_path, 'line' => $line, 'function' => $function, 'args' => $args] = $info[2];
+                $argss = [];
+                foreach ($args as $arg) {
+                    if (is_object($arg)) {
+                        $argss[] = 'object';
+                    } else if (is_string($arg)) {
+                        $argss[] = "\"{$arg}\"";
+                    } else if (is_numeric($arg)) {
+                        $argss[] = "{$arg}";
+                    } else if (is_null($arg)) {
+                        $argss[] = "null";
+                    } else if (is_bool($arg)){
+                        $argss[] = $arg?"true":"false";
+                    } else{
+                        try {
+                            $argss[] = (string)$arg;
+                        } catch (\Exception $exception) {
+                            $argss[] = 'Undefined';
+                        }
+                    }
+                }
+                $argss = implode(',', $argss);
+                $func  = "[func:{$function}($argss)]";
             }
+            $func .= "[line:{$info[1]['line']}]";
         }
         $data = $arguments[0];
-        if (!is_string($arguments[0])&&!is_numeric($arguments[0])){
-            $data = "⤵️".PHP_EOL.var_export($arguments[0],true);
+        if (!is_string($arguments[0]) && !is_numeric($arguments[0])) {
+            $data = "⤵️" . PHP_EOL . var_export($arguments[0], true);
         }
 
-        Cli::$name("[{$name}]{$func} ".$data);
+        Cli::$name("[{$name}]{$func} " . $data);
     }
 
     public static function __callStatic($name, $arguments)
     {
-        self::_call($name,$arguments);
+        self::_call($name, $arguments);
     }
 
 }
